3736e207de61663951c55344dae9dece0f6df5e9
Brian <bmascitello@gmail.com>
2017-12-11 18:35:35 -0700

    Finished Day 17 2015

---
+++ Advent of Code 2015/Day 17 2015/Day17Q1 2015.py
@@ -0,0 +1,40 @@
+"""
+Author:     Brian Mascitello
+Date:       12/11/2017
+Websites:   http://adventofcode.com/2015/day/17
+Info:       --- Day 17: No Such Thing as Too Much ---
+"""
+
+import itertools
+
+
+def get_data(input_text):
+    with open(input_text) as file:
+        data_from_file = file.read()
+    file.close()
+    return data_from_file
+
+
+puzzle_data = get_data('Day17Q1 2015 Input.txt')
+
+containers = list()
+for line in puzzle_data.splitlines():
+    containers.append(int(line))
+containers = sorted(containers)
+
+goal = 150
+minimum_containers = goal // max(containers)
+
+summation = 0
+maximum_containers = 0
+while summation < goal:
+    summation += containers[maximum_containers]
+    maximum_containers += 1
+
+containers_combo_list = list()
+for length in range(minimum_containers, maximum_containers):
+    for combo in itertools.combinations(containers, length):
+        if sum(combo) == goal:
+            containers_combo_list.append(combo)
+
+print(len(containers_combo_list))  # 1304
