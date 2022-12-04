51d40bbd7301de219c048b5ffa446cb7a0667278
Brian <bmascitello@gmail.com>
2022-12-03 23:31:01 -0700

    Finished Day 4 2022 Part 2.

--- Advent of Code 2022/Day 4 2022/Day4Q2 2022.py
+++ Advent of Code 2022/Day 4 2022/Day4Q2 2022.py
@@ -1,8 +1,9 @@
 """
 Author:     Brian Mascitello
 Date:       12/3/2022
-Websites:   https://adventofcode.com/2022/day/4
+Websites:   https://adventofcode.com/2022/day/4#part2
 Info:       --- Day 4: Camp Cleanup ---
+            --- Part Two ---
 """
 
 
@@ -15,22 +16,25 @@ def get_data(input_text):
 def main():
     data = get_data('Day4Q1 2022 Input.txt')
 
-    #     section_assignments = """2-4,6-8
+    # section_assignments = """2-4,6-8
     # 2-3,4-5
     # 5-7,7-9
     # 2-8,3-7
     # 6-6,4-6
     # 2-6,4-8"""
 
-    fully_contained_counter = 0
+    overlap_counter = 0
     for pair in data.splitlines():
         section = pair.split(',')
         x0, y0 = map(int, section[0].split('-'))
         x1, y1 = map(int, section[1].split('-'))
-        if (x0 <= x1 and y0 >= y1) or (x0 >= x1 and y0 <= y1):
-            fully_contained_counter += 1
+        section0_ints = set(range(x0, y0 + 1))
+        section1_ints = set(range(x1, y1 + 1))
+        overlap_len = len(section0_ints.intersection(section1_ints))
+        if overlap_len:
+            overlap_counter += 1
 
-    print(f'There are {fully_contained_counter} assignment pairs that fully contain the other?')
+    print(f'There are {overlap_counter} assignment pairs that fully contain the other?')
 
 
 if __name__ == '__main__':
